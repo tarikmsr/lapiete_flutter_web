6c798f2234aec912269c263809edbef9d18a92dd
Tarik EL MSAOURI <tarikmsr8@gmail.com>
2024-06-26 17:31:39 +0100

    update ip a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,8 +39,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "9d95addb9c2ce1fe991c3f5b52287dea
 "icons/Icon-512.png": "330c54ae711d5e6f7717c829c9476387",
 "icons/Icon-maskable-192.png": "f494ad20fdbee681ffaeaa2762b08aed",
 "icons/Icon-maskable-512.png": "330c54ae711d5e6f7717c829c9476387",
-"index.html": "ea79fe1a1bf81daf8cec4b64f8c145f7",
-"/": "ea79fe1a1bf81daf8cec4b64f8c145f7",
+"index.html": "049944ec6e8ece77eb5f1a748198f513",
+"/": "049944ec6e8ece77eb5f1a748198f513",
 "main.dart.js": "7922c186b522f9945c59cda881ad3fb6",
 "manifest.json": "72fed26287f1a166117cf95b70a93d9b",
 "version.json": "6657a57bd5b25519855226be5c198118"};
